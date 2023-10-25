9fcfd01964e84bb58d904962172da61b19d60da9
linn <linncoffeee@gmail.com>
2023-10-25 20:56:18 +0630

    artisan make files for Category & Item

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::resource('category', CategoryController::class);
+    Route::resource('item', ItemController::class);
 });
 
 Route::get('/dashboard', function () {
