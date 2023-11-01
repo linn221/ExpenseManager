5d6f595f442f742388ad955797c21a8a8b922d74
linn <linncoffeee@gmail.com>
2023-11-01 23:01:22 +0630

    create component for status

--- resources/views/components/session-status.blade.php
+++ resources/views/components/session-status.blade.php
@@ -1,7 +1,9 @@
-@props(['status'])
-
-@if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-lg px-3 py-2 text-green-600']) }}>
-        {{ $status }}
-    </div>
-@endif
+@props(['status', 'raw_html' => null])
+<div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+    <p class="font-bold">{{ $status }}</p>
+    @if ($raw_html)
+        <p class="font-bold">
+            {!! $raw_html !!}
+        </p>
+    @endif
+</div>
