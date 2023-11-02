99af7136fb77cc8a2cf7cec1f1403edf26b82974
linn <linncoffeee@gmail.com>
2023-11-02 15:39:03 +0630

    change tailwind config

--- resources/views/components/session-status.blade.php
+++ resources/views/components/session-status.blade.php
@@ -1,4 +1,5 @@
 @props(['status', 'raw_html' => null])
+@if ($status)
 <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
     <p class="font-bold">{{ $status }}</p>
     @if ($raw_html)
@@ -7,3 +8,5 @@
         </p>
     @endif
 </div>
+    
+@endif
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+//       content: [
+//     "./resources/**/*.blade.php",
+//     "./resources/**/*.js",
+//     "./resources/**/*.vue",
+//   ]
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
